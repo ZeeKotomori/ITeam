475c2b4592952aa3ae291b79e77923d2cfbfd16a
ZeeKotomori <zikriaf23@gmail.com>
2024-02-01 20:05:34 +0700

    add user controller func

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Siswa;
+use App\Models\User;
 use App\Models\Produk;
 use App\Models\Likes;
 use App\Models\Komentar;
 
 class UserController extends Controller
 {
+    public function index(Request $request){
 
+    }
+    public function logIn(Request $request)
+    {
+
+    }
+
+    public function signUp(Request $request)
+    {
+        $validatedData = $request->validate([
+            'email' => 'required|string|max:255|min:11',
+            'nama' => 'required|string|max:255',
+            'password' => 'required|string|max:255|min:8',
+        ]);
+
+        User::create($validatedData);
+
+        return redirect('/')->with('success', 'Data berhasil disimpan.');
+    }
 }
