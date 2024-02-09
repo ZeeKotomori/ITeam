7fbb29b388bd0fa2ef76d0bc3ec2c7179b894cac
ZeeKotomori <zikriaf23@gmail.com>
2024-02-09 17:52:49 +0700

    biar cantik aja

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable
 {
     use HasFactory;
     protected $table = 'user';
-    protected $fillable = ['id', 'email', 'password', 'nama', 'jenis_kelamin', 'no_telp', 'roles'];
+    protected $fillable = [
+        'id',
+        'email',
+        'password',
+        'nama',
+        'jenis_kelamin',
+        'no_telp',
+        'roles'
+    ];
 }
