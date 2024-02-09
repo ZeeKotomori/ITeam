465243ff8c89e5f21b059d62a93776242cd13927
ZeeKotomori <zikriaf23@gmail.com>
2024-02-09 17:50:32 +0700

    Create AuthController for Login, Logout and singUp

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) {
+        $user = User::all();
+        return view('logIn', ['user' => $user]);
+    }
+
+    public function userData(Request $request){
+        $request->validate([
+            'email' => 'required|string|max:255|min:11',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if(Auth::attempt($data)){
+            return redirect()->route('user.index');
+        }else{
+            return back()->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+            ])->onlyInput('email');
+        };
+    }
+
+    public function signUp(){
+        return view('signUp');
+    }
+    public function createData(Request $request)
+    {
+        $validatedData = $request->validate([
+            'email' => 'required|string|max:255|min:11|unique:user,email',
+            'password' => 'required|string|min:8',
+            'nama' => 'required|string|min:3',
+            'jenis_kelamin' => 'required|string',
+            'no_telp' => 'required|string|max:255|min:12',
+        ],[
+            "email.required" => "Email must be filled in",
+            "password.required" => "Password must be filled in",
+            "nama.required" => "Name must be filled in",
+            "jenis_kelamin.required" => "Gender must be filled in",
+            "no_telp.required" => "Phone must be filled in",
+        ]);
+
+        $hashedPassword = bcrypt($validatedData['password']);
+
+        $validatedData['password'] = $hashedPassword;
+
+        User::create($validatedData);
+        return redirect('/logIn')->with('success', 'Account created successfully. Please log in.');
+    }
+    public function logOut(Request $request){
+        Auth::logout();
+        return redirect()->route('logIn');
+    }
+}
