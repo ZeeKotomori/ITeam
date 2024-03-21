e43c2065b9f1fa9160201cb0ee17e91b1a894cbe
ZeeKotomori <zikriaf23@gmail.com>
2024-03-21 16:15:12 +0700

    import and use NotifAble, UUID, rename user to users, add email_verified_at on users table, add user_id on hasOne, add activeOTP funct

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,32 +2,43 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class User extends Authenticatable
 {
+    use Notifiable;
     use HasFactory;
-    protected $table = 'user';
+    use HasUuids;
+    protected $table = 'users';
     protected $fillable = [
         'id',
         'email',
         'password',
         'nama',
         'jenis_kelamin',
+        'email_verified_at',
         'no_telp',
         'roles'
     ];
-    
+
     public function Likes() : HasOne
     {
-        return $this->hasOne(Likes::class);
+        return $this->hasOne(Likes::class, 'user_id');
     }
 
     public function Comment() : HasMany
     {
         return $this->hasMany(Komentar::class);
     }
+    public function activeOTP()
+    {
+        return $this->hasOne(UserOTP::class,'user_id')->where('expired_at','>', 'now()');
+    }
 }
+
