ce3a1ad10236ce9f8afd4a68815ff0b5e4567f23
ZeeKotomori <zikriaf23@gmail.com>
2024-03-21 16:10:23 +0700

    add, guest page, commnet fucnt, likes funct, link funct

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,10 +7,19 @@ use App\Models\Produk;
 use App\Models\Likes;
 use App\Models\Komentar;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class UserController extends Controller
 {
-    public function index(Request $request) {
+    public function guest(){
+        $produk = Produk::all();
+        return response()->view('guest.index',
+        [
+            'produk' => $produk
+        ]);
+    }
+
+    public function index(){
         $produk = Produk::all();
         return response()->view('userGuest.index',
         [
@@ -18,11 +27,47 @@ class UserController extends Controller
         ])->header('Cache-Control', 'no-cache, no-store, must-revalidate')->header('Pragma', 'no-cache')->header('Expires', '0');
     }
 
-    public function comment(Request $request){
-        dd($request->all());
-        // $validasiComment = $request->validate([
-        //     'comment' => 'required',
-        // ]);
-        // $Komentar = new Komentar();
+    public function comment(Request $request, Produk $produk){
+        $request->validate([
+            'komentar' => 'required',
+        ]);
+
+        $komentarUser = $request->komentar;
+        $user_id = auth()->user()->id;
+
+        $Komentar = new Komentar();
+        $Komentar->produk_id = $produk->id;
+        $Komentar->user_id = $user_id;
+        $Komentar->komentar = $komentarUser;
+        $Komentar->save();
+
+        return redirect()->back();
+    }
+
+    public function like(Produk $produk){
+    $user_id = auth()->user()->id;
+
+    $existingLike = Likes::where('user_id', $user_id)
+                            ->where('produk_id', $produk->id)
+                            ->first();
+    if($existingLike){
+        $existingLike->delete();
+    } else {
+        $like = new Likes();
+        $like->produk_id = $produk->id;
+        $like->user_id = $user_id;
+        $like->save();
+    }
+
+    // Mengambil jumlah like terkini
+    $likesCount = Likes::where('produk_id', $produk->id)->count();
+
+    // Mengembalikan respons dalam format JSON
+    return response()->json(['likesCount' => $likesCount, 'productId' => $produk->id]);
+    }
+
+    public function link($id){
+        $produk = Produk::findOrfail($id);
+        return redirect()->awas($produk->url);
     }
 }
