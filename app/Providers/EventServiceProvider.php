9026d061efcb72062ed410a2a4c61f1719d26d0e
ZeeKotomori <zikriaf23@gmail.com>
2024-03-21 16:16:46 +0700

    create EventService for OTP

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,12 +1,10 @@
 <?php
-
 namespace App\Providers;
-
+use App\Listeners\SendOTPEventListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
-
 class EventServiceProvider extends ServiceProvider
 {
     /**
@@ -16,22 +14,25 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         Registered::class => [
-            SendEmailVerificationNotification::class,
+            // SendEmailVerificationNotification::class,
+            SendOTPEventListener::class
         ],
     ];
-
     /**
      * Register any events for your application.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
         //
     }
-
     /**
      * Determine if events and listeners should be automatically discovered.
+     *
+     * @return bool
      */
-    public function shouldDiscoverEvents(): bool
+    public function shouldDiscoverEvents()
     {
         return false;
     }
